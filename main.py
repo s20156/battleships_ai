dfdb3190051e62f84700841afedde860677fe707
Mateusz Grube <s20156@pjwstk.edu.pl>
2022-10-08 18:30:48 +0200

    Added func placeholder to add ship

--- main.py
+++ main.py
@@ -5,15 +5,27 @@ class Battleships ():
         self.players = players
         self.board_1 = [[0 for x in range(6)] for y in range(6)]
         self.board_2 = [[0 for x in range(6)] for y in range(6)]
+        self.hits_1 = []
+        self.hits_2 = []
+        self.ships_1 = []
+        self.ships_2 = []
     
-    def getBoard(self):
-        print(self.board_1)
+    def get_board(self):
+        for row in self.board_1:
+            print(row)
         print(self.board_2)
+
+    def add_ship(self, ship_class, player, position):
+        if ship_class > 1:
+            pass
+        else:
+            self.board_1[position[0]][position[1]] = 1 
 
 
 def main():
     battleships = Battleships(["1", "2"])
-    battleships.getBoard()
+    battleships.add_ship(1, 1, (1,3))
+    battleships.get_board()
 
 main()
 
