ef6fb3159caa6789a2546248fcc37a659351f469
Mateusz Grube <s20156@pjwstk.edu.pl>
2022-10-08 18:05:12 +0200

    Added boards

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from easyAI import TwoPlayersGame, Human_Player, AI_Player
+from easyAI import TwoPlayerGame, Human_Player, AI_Player
 
-class Battleships ( TwoPlayersGame ):
-    __init__(self, players):
+class Battleships ():
+    def __init__(self, players):
         self.players = players
+        self.board_1 = [[0 for x in range(6)] for y in range(6)]
+        self.board_2 = [[0 for x in range(6)] for y in range(6)]
+    
+    def getBoard(self):
+        print(self.board_1)
+        print(self.board_2)
 
 
+def main():
+    battleships = Battleships(["1", "2"])
+    battleships.getBoard()
+
+main()
 
 
