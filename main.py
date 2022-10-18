28676090da64f274e71ffa8e6e0d758e603f5632
cezarkoom <Cezar9719@gmail.com>
2022-10-18 21:17:16 +0200

    Added abstract methods for TwoPlayerGame req

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from easyAI import TwoPlayerGame, Human_Player, AI_Player
+from easyAI import TwoPlayerGame
 
-class Battleships ():
+
+class Battleships(TwoPlayerGame):
+
     def __init__(self, players):
         self.players = players
         self.board_1 = [[0 for x in range(6)] for y in range(6)]
@@ -9,11 +11,23 @@ class Battleships ():
         self.hits_2 = []
         self.ships_1 = []
         self.ships_2 = []
-    
+
+    def possible_moves(self):
+        print("Choose the field and click on in to shoot")
+        pass
+
+    def make_move(self, move):
+        self.shoot()
+        pass
+
+    def is_over(self):
+        return self.win()
+        pass
+
     def get_board(self):
         for row in self.board_1:
             print(row)
-        print ("   ====   ")
+        print("   ====   ")
         for row in self.board_2:
             print(row)
 
@@ -21,18 +35,18 @@ class Battleships ():
         if ship_class > 1:
             pass
         else:
-            self.board_1[position[0]][position[1]] = 1 
+            self.board_1[position[0]][position[1]] = 1
 
-    
     def add_big_ship(self, ship_class, player, position_start, position_end):
-        if abs(position_end[0] - position_start[0]) != ship_class - 1 and abs(position_end[1] - position_start[1]) != ship_class - 1:
+        if abs(position_end[0] - position_start[0]) != ship_class - 1 and abs(
+                position_end[1] - position_start[1]) != ship_class - 1:
             return
 
         self.board_1[position_start[0]][position_start[1]] = 1
         self.board_1[position_end[0]][position_end[1]] = 1
-        
+
         print(position_start[1], position_end[1])
-        
+
         if position_start[0] != position_end[0]:
             if position_start[0] > position_end[0]:
                 self.board_1[position_start[0] - 1][position_start[1]] = 1
@@ -43,11 +57,13 @@ class Battleships ():
                 self.board_1[position_start[0]][position_start[1] - 1] = 1
             else:
                 self.board_1[position_start[0]][position_end[1] + 1] = 1
-        
 
     def shoot(self, position, player):
         if self.board_1[position[0]][position[1]] == 1:
             self.board_1[position[0]][position[1]] = 2
+
+    def win(self):
+        return self.board_1 or self.board_2 == 0 or 2
 
 
 ##############
@@ -59,12 +75,11 @@ class Battleships ():
 
 def main():
     battleships = Battleships(["1", "2"])
-    battleships.add_ship(1, 1, (1,3))
-    battleships.add_big_ship(3, 1, (4,5), (4,3))
-    battleships.add_big_ship(2, 1, (2,2), (3,2))
-    battleships.shoot((2,2), 2)
+    battleships.add_ship(1, 1, (1, 3))
+    battleships.add_big_ship(3, 1, (4, 5), (4, 3))
+    battleships.add_big_ship(2, 1, (2, 2), (3, 2))
+    battleships.shoot((2, 2), 2)
     battleships.get_board()
 
+
 main()
-
-
